e5754a3c0476ca9379f9ff9448b379ee40d7f8a1
phillers <phillers@Phillercity>
2017-01-08 16:12:00 +0100

    poprawka

--- client/src/MyFrame.java
+++ client/src/MyFrame.java
@@ -1,11 +1,7 @@
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
-import java.io.File;
 import java.io.IOException;
-
-import javax.imageio.ImageIO;
 import javax.swing.BoxLayout;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
