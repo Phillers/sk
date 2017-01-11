98c1bf02dbad2adc06641cb54e975d116c2c8fc7
phillers <phillers@Phillercity>
2017-01-12 00:20:57 +0100

    kod

--- client/s.c
+++ client/s.c
@@ -19,7 +19,6 @@ char WRONG = 33;
 char WIN = 34;
 char LOSE = 35;
 char ERROR = 36;
-
 int n = 256;
 struct param {
   int socket;
@@ -27,24 +26,32 @@ struct param {
 
 void ex(char turn, int * player) {
 //zakończenie wątku
+  //przyjmuje parametr gracza który wygrał i tablicę socketów
+  //zamyka i zwalnia sockety
   close(player[0]);
   close(player[1]);
   free(player);
+  //i kończy wątek
   pthread_exit( & turn);
 }
 
 void error(char turn, int player[]) {
 //błąd w połączeniu
+  //przyjmuje parametr gracza który się rozłączył i tablicę socketów
   perror("error");
   printf("Connection from %d lost\n", turn);
+  //informuje drugiego gracza o utracie połączenia
   if (write(player[1 - turn], & ERROR, 1) < 1) {
     printf("Connection from %d lost\n", 1 - turn);
   }
+  //i przechodzi do zakończenia wątku
   ex(turn, player);
 }
 
 char wygrana(char test[]){
 //sprawdza zbiór pól
+  //przyjmuje w parametrze 4 pola
+  //sprawdza czy mają jakiś wspólny bit i jak tak to zwraca 1
       char and = 15;
       char or = 0;
       int i;
@@ -53,14 +60,61 @@ char wygrana(char test[]){
         or |= test[i];
       }
       if (and > 0 || or < 15) return 1;
+  return 0;
 }
 
+char sprawdz_wygrana(char wiersze[], char wiersz, char kolumny[],char kolumna,char przekatne[],char pola[4][4]){
+  //zapisuje zmianę stanu planszy i sprawdza ewentualne możliwości wygrania
+  //w parametrze dane o planszy i ruchu
+
+  //zwiekszenie licznika liczby pól w rzędach
+  wiersze[wiersz]++;
+  kolumny[kolumna]++;
+  //i przekątnych
+  if (wiersz == kolumna)przekatne[0]++;
+  if (wiersz + kolumna == 3) przekatne[1]++;
+
+  //sprawdzenie pełnych rzędów
+  //jeśli jakiś jest pełny zapisanie do zbioru testowego
+  //i wywołanie funkcji wygrana
+  char test[4];
+  char wygr=0;
+  int x;
+  if (wiersze[wiersz] == 4) {
+    for(x=0;x<4;x++)
+      test[x]=pola[wiersz][x];
+    if(wygrana(test))wygr=1;
+  }
+  if (kolumny[kolumna] == 4) {
+    for (x = 0; x < 4; x++)
+      test[x]= pola[x][kolumna];
+    if(wygrana(test))wygr=1;
+  }
+  if (przekatne[0] == 4) {
+    for (x = 0; x < 4; x++)
+      test[x]= pola[x][x];
+    if(wygrana(test))wygr=1;
+  }
+  if (przekatne[1] == 4) {
+    for (x = 0; x < 4; x++)
+      test[x]= pola[x][3 - x];
+    if(wygrana(test))wygr=1;
+  }
+  return wygr;
+}
+
+
 void * dzialanie(void * clientSocket) {
+  //w parametrze przyjmuje tablicę z socketami
+  //zmienne do obsługi gry
+  int * player = (int * ) clientSocket;
+  int turn = 0;
   char pionek;
   char pion;
   char pole;
   char pola[4][4];
   bzero(pola, 16);
+  //i dane o stanie planszy
   char wiersze[4];
   bzero(wiersze, 4);
   char kolumny[4];
@@ -69,8 +123,7 @@ void * dzialanie(void * clientSocket) {
   bzero(przekatne, 2);
   char wiersz;
   char kolumna;
-  int * player = (int * ) clientSocket;
-  int turn = 0;
+
   int x;
 
   while (1) {
@@ -108,44 +161,17 @@ void * dzialanie(void * clientSocket) {
     pola[wiersz][kolumna] = pion;
 
     //sprawdzenie czy ktoś wygrał
-    //zapisanie zmian na planszy
-    wiersze[wiersz]++;
-    kolumny[kolumna]++;	
-    //sprawdzenie przekątnych
-    if (wiersz == kolumna)przekatne[0]++;
-    if (wiersz + kolumna == 3) przekatne[1]++;
+    char wygr=sprawdz_wygrana(wiersze,wiersz,kolumny,kolumna,przekatne,pola);
     
-    //sprawdzenie pełnych rzędów    
-    char test[4];
-    char wygr=0;
-    if (wiersze[wiersz] == 4) {
-      for(x=0;x<4;x++)
-        test[x]=pola[wiersz][x];
-      if(wygrana(test))wygr=1;
-    }
-    if (kolumny[kolumna] == 4) {
-      for (x = 0; x < 4; x++)
-        test[x]= pola[x][kolumna];
-      if(wygrana(test))wygr=1;
-    }
-    if (przekatne[0] == 4) {
-      for (x = 0; x < 4; x++) 
-        test[x]= pola[x][x];      
-      if(wygrana(test))wygr=1;
-    }
-    if (przekatne[1] == 4) {
-      for (x = 0; x < 4; x++)
-        test[x]= pola[x][3 - x];
-      if(wygrana(test))wygr=1;
-    }
-    
-    //jesli spełniony któryś warunek to odpowiednie komunikaty
+    //jesli tak
     if (wygr) {
       printf("Wygrał gracz %d\n", 1 - turn + 1);
+      //wysłanie do graczy komunikatów o wygranej i przegranej
       if (write(player[turn], &LOSE, 1) < 0)
         error(turn, player);
       if (write(player[1 - turn], &WIN, 1) < 0)
         error(1 - turn, player);
+      //zakończenie wątku
       ex(turn, player);
     }
     turn = 1 - turn;
